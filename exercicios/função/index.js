bf41c14e7ed60227cb4f6c9bcee68720313ac026
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2023-10-11 15:19:47 -0300

    exercicios

---
+++ exercicios/função/index.js
@@ -0,0 +1,16 @@
+
+function incentivo(nome = 'Quester' ){
+    alert('Muito bem ' + nome + ', você chegou ao JS, continue assim!')
+}
+
+incentivo('Daniel')
+/*
+function soma(n1, n2){
+    return n1 + n2
+}
+
+let resultadoSoma = soma(5, 5)
+console.log(resultadoSoma)
+
+resultadoSoma = soma(resultadoSoma, 5)
+console.log(resultadoSoma) */
---
+++ exercicios/variaveis/index.js
@@ -0,0 +1,4 @@
+
+const prompt('Digite seu nome: ')
+let nome = (prompt.value)
+alert('Bem vindo ' + nome + ', bons estudos na sua caminhada!')
