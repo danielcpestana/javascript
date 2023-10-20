c208e9243928be171565eb77b04c219a7e9ac3d6
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2023-10-20 15:31:01 -0300

    att

---
+++ exercicios/22-resolvendo-erros-js/index.js
@@ -0,0 +1,8 @@
+let inputTelefone = document.getElementById('telefone')
+
+inputTelefone.disabled = true
+
+function habilitarTelefone(){
+    let inputTelefone = document.getElementById('telefone')
+    inputTelefone.disabled = false
+}
