5a104f831a6919922b99cb7c08e18e3bbfc7ecc2
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2023-10-24 17:13:58 -0300

    string

---
+++ exercicios/26 - interpolacao-de-string/index.js
@@ -0,0 +1,5 @@
+const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+    return `Primeiro Nome: ${primeiroNome}, Último Nome: ${ultimoNome}, Idade:  ${idade}`
+}
+
+console.log(`${imprimirInformacoesPessoa("Daniel","Pestana" , 22)} Eu vou ser dev!`)
