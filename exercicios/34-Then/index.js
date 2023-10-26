35d32ccdadd128ba97e7f9b45f79fd7560974275
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2023-10-26 17:43:55 -0300

    ex

---
+++ exercicios/34-Then/index.js
@@ -0,0 +1,21 @@
+let ferverAgua = (chaleiraEstaNoFogao, fogaoEstaLigado) => {
+    return new Promise((resolve, reject) => {
+        if(chaleiraEstaNoFogao && fogaoEstaLigado){
+            console.log('começando o processo de ferver água')
+            resolve()
+        }else {
+            console.log('é necessário lgiar o fogão e colocar a chaleira no fogão para ferver a água')
+            reject()
+        }
+    }
+        )
+    
+}
+
+let chaleiraEstaNoFogao = true
+let fogaoEstaLigado = true
+
+let passarCafe = () => console.log('passando café')
+
+ferverAgua(chaleiraEstaNoFogao, fogaoEstaLigado).then(passarCafe)
+console.log('fazendo café')
