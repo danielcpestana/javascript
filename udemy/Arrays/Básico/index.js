336b5930586b4298f9959f771015b621803ec4ea
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2024-03-28 18:09:26 -0300

    exercicios

---
+++ udemy/Arrays/Básico/index.js
@@ -0,0 +1,21 @@
+const alunos = ['Luiz', 'Maria', 'João']
+
+
+
+console.log(alunos)
+
+
+
+
+//const removido = alunos.shift() // remove no começo
+//alunos.pop() //remove no final
+
+//alunos.unshift('Luiza') //Adiciona no começo
+//alunos.unshift('Fábio')
+
+
+//alunos.push('Daniel')  //Adiciona no fim
+//alunos.push('Fábio')
+
+
+
