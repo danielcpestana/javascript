d121bfe80d281a1b6a80c79f0d55ff25f7b7d911
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2024-04-26 18:11:33 -0300

    ex

---
+++ udemy/Arrays/avançado/01.js
@@ -0,0 +1,13 @@
+// Valor por referência
+//
+ const nomes = ['Daniel', 'Luiz', 'Joana', 'João', 'Gabriel']
+
+ //pop
+//  nomes.splice(-1, 1)
+// shift
+// nomes.splice(0, 1)
+//push
+// nomes.splice(nomes.length, 0, 'Abel')
+//unshift
+// nomes.splice(0, 0, 'Charles')
+console.log(nomes)
